610140a4111984334a4357cca18ae6801a286898
Vithushan <sutharshanvithushan@gmail.com>
2026-01-31 00:34:36 +0530

    script to search on dumb

---
+++ dumb.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+if [[ "$1" != "" ]]; then
+	if [[ "$1" == "Cancel" ]]; then
+		exit 0
+	fi
+	search="$(echo "$1" | jq -Rr @uri)"
+	notify-send "Opening Dumb lyrics search for \"$1\" in qutebrowser"
+	qutebrowser "http://lollab:5555/search?q=$search"
+	exit 0
+fi
+echo "Cancel"
+mpc listall --format="%artist% %title%"
