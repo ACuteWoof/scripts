457f3754fb821f2cb0ce8090f9d7c1add45e612e
Vithushan <sutharshanvithushan@gmail.com>
2026-02-01 10:16:33 +0530

    combine it all into entr-runner

--- server/entr-runner.sh
+++ server/entr-runner.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-find /home/acutewoof/gitbrowse/gitbrowse | entr -n /home/acutewoof/update-gitbrowse.sh &
-find /home/acutewoof/gitbrowse | entr -n /home/acutewoof/github-update.sh
+find /home/acutewoof/gitbrowse/me/gitbrowse | entr -n /home/acutewoof/update-gitbrowse.sh &
+
+find /home/acutewoof/gitbrowse -type f -path "*/refs/heads/*" | while IFS= read -r file; do
+    repo_dir=$(dirname "$(dirname "$(dirname "$file")")")
+    echo "$file" | entr -n -s "cd '$repo_dir' && su acutewoof -c 'git push github --all --force'" &
+done
+
+wait
--- server/github-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-cd /home/acutewoof/gitbrowse;
-
-for dir in /home/acutewoof/gitbrowse/*;
-do 
-	[ -d "$dir" ] && cd "$dir" && su acutewoof -c "git push github master"
-done;
