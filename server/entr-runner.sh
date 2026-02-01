419fe673178f7260365e3e66fde0f559d2811f54
Vithushan <sutharshanvithushan@gmail.com>
2026-02-01 10:49:14 +0530

    mirror for entr runner

--- server/entr-runner.sh
+++ server/entr-runner.sh
@@ -4,7 +4,7 @@ find /home/acutewoof/gitbrowse/me/gitbrowse | entr -n /home/acutewoof/update-git
 
 find /home/acutewoof/gitbrowse -type f -path "*/refs/heads/*" | while IFS= read -r file; do
     repo_dir=$(dirname "$(dirname "$(dirname "$file")")")
-    echo "$file" | entr -n -s "cd '$repo_dir' && su acutewoof -c 'git push github --all --force'" &
+    echo "$file" | entr -n -s "cd '$repo_dir' && su acutewoof -c 'git fetch --all && git push github --force --mirror'" &
 done
 
 wait
