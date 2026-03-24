ee9a01c9f3ed13f121ccdcd1c4b25a3da6495d3f
acutewoof <sutharshanvithushan@gmail.com>
2026-03-24 23:26:43 +0530

    ignore .git files and node modules in file finding

--- find-file.sh
+++ find-file.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find ~ | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find file:' -l 10 | xargs -r xdg-open
+find ~ ! -path "*.git*" ! -path "*node_modules*" | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find file:' -l 10 | xargs -r xdg-open
--- find-notes.sh
+++ find-notes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ~/basement/notes  | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find notes:' -l 10 | xargs -r xdg-open
+find ~/basement/notes ! -path "*.git*" ! -path "*node_modules*" | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find notes:' -l 10 | xargs -r xdg-open
 
