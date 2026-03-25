87d49b73eb503fcc15c90d3804207f670e955dd7
acutewoof <sutharshanvithushan@gmail.com>
2026-03-25 13:14:35 +0530

    cd

--- find-notes.sh
+++ find-notes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find ~/basement/notes ! -path "*.git*" ! -path "*node_modules*" | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find notes:' -l 10 | xargs -r xdg-open
+cd ~/basement/notes && find . ! -path "*.git*" ! -path "*node_modules*" | sed -e "s/'/\\\'/g" -e 's/\ /\\ /g' | dmenu -i -p 'Find notes:' -l 10 | xargs -r xdg-open
 
