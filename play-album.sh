57e8492be036cd55814c7ffc6f2b41236832537c
acutewoof <sutharshanvithushan@gmail.com>
2026-03-25 22:41:20 +0530

    fix naming

--- play-album.sh
+++ play-album.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mpc ls | dmenu -i -p 'Play album:' -l 10 | xargs -0 -I @ sh -c 'mpc clear && mpc add "@" && mpc play'
+mpc ls | dmenu -i -p 'Play album:' -l 10 | sed -e "s|'|\\\\'|g" -e 's|"|\"|g' | xargs -r -I @ sh -c 'mpc clear && mpc add "@" && mpc play'
