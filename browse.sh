283326f9b97c537827f7bf8d87f8c95440a9a87e
acutewoof <sutharshanvithushan@gmail.com>
2026-03-25 22:32:01 +0530

    fix xargs

--- browse.sh
+++ browse.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grep http ~/basement/bookmarks.txt | dmenu -i -p 'Open qutebrowser:' -l 10 | xargs -I @ qutebrowser "@"
+grep http ~/basement/bookmarks.txt | dmenu -i -p 'Open qutebrowser:' -l 10 | xargs -I @ -0 qutebrowser "@"
--- play-album.sh
+++ play-album.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mpc ls | dmenu -i -p 'Play album:' -l 10 | xargs -I @ sh -c 'mpc clear && mpc add "@" && mpc play'
+mpc ls | dmenu -i -p 'Play album:' -l 10 | xargs -0 -I @ sh -c 'mpc clear && mpc add "@" && mpc play'
